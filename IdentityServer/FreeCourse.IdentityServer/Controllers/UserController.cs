89bc647b58359eb0b009876ad4f758b4c641435a
Serkan OZEL <serkan.ozel@bilgeadam.com>
2023-03-23 00:43:15 +0300

    IdentityServer Microservice Added

---
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,67 @@
+﻿using FreeCourse.IdentityServer.Dtos;
+using FreeCourse.IdentityServer.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace FreeCourse.IdentityServer.Controllers
+{
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto signUp)
+        {
+            var user = new ApplicationUser()
+            {
+                UserName = signUp.UserName,
+                Email = signUp.Email,
+                City = signUp.City,
+            };
+            var result = await _userManager.CreateAsync(user, signUp.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors.Select(x => x.Description).ToList());
+            }
+
+            return NoContent();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Sub);
+
+            if (userIdClaim is null)
+                return BadRequest();
+
+            var user = await _userManager.FindByIdAsync(userIdClaim.Value);
+
+            if (user == null)
+                return BadRequest();
+
+            return Ok(new
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                Email = user.Email,
+                City = user.City
+            });
+        }
+    }
+}
--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,6 +1,8 @@
 using FreeCourse.Services.Catalog.Services.CategoryServices;
 using FreeCourse.Services.Catalog.Services.CourseServices;
 using FreeCourse.Services.Catalog.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +12,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICourseService, CourseService>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,6 +29,13 @@ builder.Services.AddSingleton<IDatabaseSettings>(opt =>
     return opt.GetRequiredService<IOptions<DatabaseSettings>>().Value;
 });
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerURL"];
+    options.Audience = "resource_catalog";
+    options.RequireHttpsMetadata = false;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,6 +44,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
